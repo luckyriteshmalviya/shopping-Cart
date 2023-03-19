089342969b60b0b5ad3846acda8db3a36167975d
Ritesh <luckyriteshmalviya@gmail.com>
2023-03-19 21:52:15 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./app.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import Header from "./components/mainComponents/header/header";
