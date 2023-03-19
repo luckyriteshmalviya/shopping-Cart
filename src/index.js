ad7172ba9beddceb65e869ce5c82fc8af43a9f80
Ritesh <luckyriteshmalviya@gmail.com>
2023-03-19 20:43:31 +0530

    project completed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import App from "./app";
+import App from "./App";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import reportWebVitals from "./reportWebVitals";
