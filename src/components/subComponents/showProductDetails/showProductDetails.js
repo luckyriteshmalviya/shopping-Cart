6f5e514ecf08c02f736c4ccf79b3cf7dbe12b090
Ritesh Malviya <99033374+luckyriteshmalviya@users.noreply.github.com>
2023-04-25 18:59:10 +0530

    Update showProductDetails.js

--- src/components/subComponents/showProductDetails/showProductDetails.js
+++ src/components/subComponents/showProductDetails/showProductDetails.js
@@ -5,12 +5,12 @@ const ShowProductDetails = ({ item }) => {
     <>
       <div className="sample-detail-1">
         <div className="sample-detail-1-title">{item.title}</div>
-        <div className="rating">{item.rating}</div>
+        <div className="rating">{item.rating}{"\u2605"}</div>
       </div>
 
       <div className="sample-detail-2">
         <div>Price</div>
-        <div>{item.price}</div>
+        <div>{"\u20B9"}{item.price}</div>
       </div>
 
       <div className="sample-detail-3">
