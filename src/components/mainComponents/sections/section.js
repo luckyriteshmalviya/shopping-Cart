c511d1fdb3d2e7aaa67d4c5545400dc70b262b94
Ritesh Malviya <99033374+luckyriteshmalviya@users.noreply.github.com>
2023-04-28 18:30:25 +0530

    Update section.js

--- src/components/mainComponents/sections/section.js
+++ src/components/mainComponents/sections/section.js
@@ -1,6 +1,6 @@
-import { useEffect, lazy, Suspense } from "react";
+import { useEffect, lazy, Suspense, useState } from "react";
 import "./section.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { productDetails } from "../../../redux/productSlice";
 import CartList from "../cartList/cartList";
 import { Link } from "react-router-dom";
@@ -10,10 +10,23 @@ const ShowProduct = lazy(() => import("../showProduct/showProduct.js"));
 
 const Section = () => {
   const dispatch = useDispatch();
+  const [products, setProducts] = useState();
+  const selector = useSelector((state) => state.productSlice.productDetails);
+
+  const [categories, setCategories] = useState("All Categories");
+
+  const handleFilterChange = (e) => {
+    setCategories(e.target.value);
+  };
 
   useEffect(() => {
     dispatch(productDetails());
   }, []);
+
+  useEffect(() => {
+    let allP = selector.filter((item) => item.category===categories);
+    setProducts(allP);
+  }, [categories]);
 
   return (
     <>
@@ -21,7 +34,20 @@ const Section = () => {
         <abbr className="section-one-header-left-part" title="coming soon...">
           <div>Filter</div>
           <div> / </div>
-          <div>Sort</div>
+          <select
+            className="section-one-header-left-part-list"
+            onChange={handleFilterChange}
+          >
+            <option>All</option>
+            <option>smartphones</option>
+            <option>laptops</option>
+            <option>fragrances</option>
+            <option>skincare</option>
+            <option>groceries</option>
+            <option>home-decoration</option>
+            <option>furniture</option>
+            <option>tops</option>
+          </select>
         </abbr>
         <Link className="section-one-go-to-cart-link" to={"/total"}>
           Go to Shoping Cart
@@ -32,7 +58,7 @@ const Section = () => {
       <div className="section-one-main-container">
         <div className="section-one-main-container-show-product">
           <Suspense fallback={<div>loading... </div>}>
-            <ShowProduct />
+            <ShowProduct productList={categories==="All Categories"? selector : products} />
           </Suspense>
         </div>
         <div className="section-one-main-container-cartlist">
